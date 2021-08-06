aab98284c4fe9b57624142ab737fe616a4ece00b
sagar-gohel <sagar.gohel597@gmail.com>
2021-08-06 21:05:59 +0530

    My_Whatsapp_Status_Savior

---
+++ app/src/main/java/com/shagii/wsstatusdownloader/Constant.java
@@ -0,0 +1,6 @@
+package com.shagii.wsstatusdownloader;
+
+public class Constant {
+    public static final String FOLDER_NAME = "/WhatsApp/";
+    public static final String SAVE_FOLDER_NAME = "/WS Status Downloder/";
+}
